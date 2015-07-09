a926e218703e480417fd1881a29bb8d90755d466
Tuba Topaloğlu <Tuba@Tuba-MacBook-Air.local>
2015-07-09 11:31:35 +0300

    ss

--- android/app/src/main/java/com/pauln/carcontrol/ButtonDispatcherInterface.java
+++
@@ -1,9 +0,0 @@
-package com.pauln.carcontrol;
-
-/**
- * Created by Pauln on 15/03/2015.
- */
-public interface ButtonDispatcherInterface
-{
-    public ButtonDispatcher getButtonDispatcher();
-}
--- android/app/src/main/java/com/pauln/carcontrol/ButtonType.java
+++
@@ -1,9 +0,0 @@
-package com.pauln.carcontrol;
-
-/**
- * Created by Pauln on 14/02/2015.
- */
-public enum ButtonType
-{
-    Touch, Toggle, Value;
-}
